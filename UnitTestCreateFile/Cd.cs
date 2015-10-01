e16f0c77cbd43b28ddf6cc1b13be014caf4cc5bc
waylandix <waylandix@gmail.com>
2015-10-01 14:20:55 +0200

    Test Unitaire Cd by directory in no exist file

--- UnitTestCreateFile/Cd.cs
+++ UnitTestCreateFile/Cd.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileSystem;
 
 namespace UnitTestCreateFile
 {
     [TestClass]
-    public class UnitTest1
+    public class Cd
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CdByDirectoryInFileNoExist()
         {
+            
+            Directory dossier = new Directory("dossier", null);
+            dossier.chmod(7);
+            dossier.createNewFile("hui");
+            Assert.AreEqual(dossier.cd("rfd"), null);
+            
         }
     }
 }
