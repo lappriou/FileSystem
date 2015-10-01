0c1017937d33433d2b99b1486bb4a18e3802a39f
waylandix <waylandix@gmail.com>
2015-10-01 15:27:45 +0200

    Test Unitaire get Path

---
+++ UnitTestCreateFile/GetPath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileSystem;
+
+namespace UnitTestCreateFile
+{
+    [TestClass]
+    public class GetPath
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
