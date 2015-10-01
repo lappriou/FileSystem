216ca207b98a6b8abe405b5bd04cd9a97cadc8a0
waylandix <waylandix@gmail.com>
2015-10-01 14:36:21 +0200

    Test Unitaire ls

---
+++ UnitTestCreateFile/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileSystem;
+
+namespace UnitTestCreateFile
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
