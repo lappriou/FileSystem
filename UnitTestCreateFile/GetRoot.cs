86b31c423af76c077a81ae839dcc09ec90c696f6
waylandix <waylandix@gmail.com>
2015-10-01 15:51:22 +0200

    Test Unitaire get Root

---
+++ UnitTestCreateFile/GetRoot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileSystem;
+
+namespace UnitTestCreateFile
+{
+    [TestClass]
+    public class GetRoot
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
