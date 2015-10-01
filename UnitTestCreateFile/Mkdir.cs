9e1f0c18c676e66f6cb97d9f9ac7848a82280b16
waylandix <waylandix@gmail.com>
2015-10-01 13:47:15 +0200

    Test Unitaire Mkdir by directory with permit

---
+++ UnitTestCreateFile/Mkdir.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileSystem;
+
+namespace UnitTestCreateFile
+{
+    [TestClass]
+    public class Mkdir
+    {
+        [TestMethod]
+        public void MkdirByFile()
+        {
+            File file = new File("file", null);
+            file.chmod(7);
+            Assert.IsFalse(file.createNewFile("hui"));
+        }
+
+        [TestMethod]
+        public void MkdirByDirectoryWithPermit()
+        {
+            Directory dossier = new Directory("dossier", null);
+            dossier.chmod(7);
+            Assert.IsTrue(dossier.createNewFile("hui"));
+        }
+    }
+}
