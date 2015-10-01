ef07d0f53ead45854f7bb1cd0162e1f64a3a2d15
waylandix <waylandix@gmail.com>
2015-10-01 17:52:26 +0200

    Test Unitaire Search file no exist

---
+++ UnitTestCreateFile/Search.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileSystem;
+
+namespace UnitTestCreateFile
+{
+    [TestClass]
+    public class Search
+    {
+        [TestMethod]
+        public void SearchbyFileNoExist()
+        {
+            Directory dossier = new Directory("dossier", null);
+            dossier.mkdir("hui");
+            dossier.chmod(7);
+            Assert.AreEqual(dossier.search("cuicui").Count, 0);
+        }
+    }
+}
