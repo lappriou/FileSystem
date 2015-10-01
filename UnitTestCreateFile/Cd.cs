56cf5a86a65e9f4e853923d88b78bd22b8344693
waylandix <waylandix@gmail.com>
2015-10-01 14:27:46 +0200

    Test Unitaire Cd by directory in file

--- UnitTestCreateFile/Cd.cs
+++ UnitTestCreateFile/Cd.cs
@@ -11,11 +11,20 @@ namespace UnitTestCreateFile
         public void CdByDirectoryInFileNoExist()
         {
             
+            Directory dossier6 = new Directory("dossier", null);
+            dossier6.chmod(7);
+            dossier6.createNewFile("hui");
+            Assert.AreEqual(dossier6.cd("rfd"), null);  
+        }
+
+        [TestMethod]
+        public void CdByDirectoryInFile()
+        {
+
             Directory dossier = new Directory("dossier", null);
             dossier.chmod(7);
             dossier.createNewFile("hui");
-            Assert.AreEqual(dossier.cd("rfd"), null);
-            
+            Assert.AreEqual(dossier.cd("hui"), dossier.ListFile[0]);
         }
     }
 }
