b8d4b5632f188fbc75ccaa0bf875a888eaa7baa8
waylandix <waylandix@gmail.com>
2015-10-01 15:59:21 +0200

    Test Unitaire get Root by file

--- UnitTestCreateFile/GetRoot.cs
+++ UnitTestCreateFile/GetRoot.cs
@@ -8,8 +8,19 @@ namespace UnitTestCreateFile
     public class GetRoot
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetRootByFile()
         {
+            Directory dossier = new Directory("C:", null);
+
+            dossier.chmod(7);
+            dossier.mkdir("hui");
+            File FileCurrent = dossier;
+            FileCurrent = dossier.cd("hui");
+            FileCurrent.chmod(7);
+            FileCurrent.mkdir("toto");
+            FileCurrent = FileCurrent.cd("toto");
+            FileCurrent.chmod(7);
+            Assert.AreEqual(FileCurrent.getRoot(), "hui");
         }
     }
 }
