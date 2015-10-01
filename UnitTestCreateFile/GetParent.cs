697441d5924ed5031ab3a0b6b9b79237dd824d6a
waylandix <waylandix@gmail.com>
2015-10-01 14:01:44 +0200

    Test Unitaire Cd

--- UnitTestCreateFile/GetParent.cs
+++ UnitTestCreateFile/GetParent.cs
@@ -7,7 +7,15 @@ namespace UnitTestCreateFile
     [TestClass]
     public class GetParent
     {
+        [TestMethod]
+        public void GetParentByFile()
+        {
 
+            Directory dossier = new Directory("dossier", null);
+            dossier.chmod(7);
+            File file = new File("moi", dossier);
+            Assert.AreEqual(file.getParent(), "dossier");
+        }
 
     }
 }
