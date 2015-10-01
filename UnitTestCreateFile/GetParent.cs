a6ab85e7605c6b1d406135dd5d5ee6f13e57dae6
waylandix <waylandix@gmail.com>
2015-10-01 13:55:43 +0200

    Test Unitaire GetParent

---
+++ UnitTestCreateFile/GetParent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileSystem;
+
+namespace UnitTestCreateFile
+{
+    [TestClass]
+    public class GetParent
+    {
+
+
+    }
+}
