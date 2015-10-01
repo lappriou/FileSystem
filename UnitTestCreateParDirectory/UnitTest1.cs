57fe163a5e20918f8d0e80d965eebc1f8f92a69a
waylandix <waylandix@gmail.com>
2015-10-01 13:14:39 +0200

    Test Unitaire Create par Directory

---
+++ UnitTestCreateParDirectory/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileSystem;
+
+namespace UnitTestCreateParDirectory
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            Directory dossier = new Directory("dossier", null);
+            dossier.chmod(7);
+            Assert.IsTrue(dossier.createNewFile("hui"));
+        }
+    }
+}
