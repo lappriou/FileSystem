e31d5c1d272b8004dc553d8aa27726cfc2d27510
waylandix <waylandix@gmail.com>
2015-10-02 11:08:27 +0200

    Test Unitaire Rename get name

---
+++ UnitTest/GetName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileSystem;
+
+namespace UnitTestCreateFile
+{
+    [TestClass]
+    public class GetName
+    {
+        [TestMethod]
+        public void GetNamebyFile()
+        {
+            Directory dossier = new Directory("dossier", null);
+            dossier.chmod(7);
+            Assert.AreEqual(dossier.getName(), "dossier");
+        }
+
+    }
+}
+
