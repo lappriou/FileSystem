6e6a7a0bfc895bbb17964f1aa0fbc512782b7696
waylandix <waylandix@gmail.com>
2015-10-02 10:20:01 +0200

    Test Unitaire Rename to by directory

---
+++ UnitTest/Chmod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileSystem;
+
+namespace UnitTestCreateFile
+{
+    [TestClass]
+    public class Chmod
+    {
+        [TestMethod]
+        public void ChmodByFile()
+        {
+            Directory dossier = new Directory("dossier", null);
+            dossier.chmod(7);
+            Assert.AreEqual(dossier.Permission, 7);
+        }
+    }
+}
---
+++ UnitTest/RenameTo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileSystem;
+
+namespace UnitTestCreateFile
+{
+    [TestClass]
+    public class RenameTo
+    {
+        [TestMethod]
+        public void RenameToByDirectory()
+        {
+            Directory dossier = new Directory("dossier", null);
+            dossier.createNewFile("hui");
+            dossier.renameTo("hui", "toto");
+            Assert.AreEqual(dossier.ListFile[0].Name , "toto");
+        }
+    }
+}
